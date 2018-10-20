3ebf36ffdc26f8274d112ad1b94050737506c3ff
Steven Venham <deathmock@gmail.com>
2018-10-20 13:36:54 -0400

    Created add user

---
+++ adduser.php
@@ -0,0 +1,68 @@
+<?php 
+	if(isset($_POST['post_users']))
+	{
+		//We have a post
+		$user = $_POST['post_users'];
+		$level = $_POST['post_level'];
+		$pass = $_POST['post_pass'];
+		
+		//Hash the password
+		$hash = password_hash($pass, PASSWORD_DEFAULT);
+		
+		$sql = "INSERT INTO users (uname,level,passhash) VALUES ($user,$level,$hash);";
+		
+		include "mysql_connect.php"; //Grants access to $mysqli-> variable.
+		
+		if(!$mysqli->query($sql))
+            {
+                echo $mysqli->error;
+            }
+		
+		$mysqli->close();
+	}
+	else
+	{
+		//display form
+		echo '<title>API Backend</title>
+  </head>
+  <body>
+  <form action="adduser.php" method="POST">
+  <table>
+	<tr>
+		<td>
+		<h3>Username:</h3>
+		</td>
+		<td>
+			<input type="text" name="post_users">
+		</td>
+	</tr>
+	<tr>
+		<td>
+    <h3>level:</h3>
+	</td>
+		<td>
+    <input type="text" name="post_level">
+		</td>
+	</tr>
+	<tr>
+		<td>
+    <h3>Password</h3>	
+	</td>
+		<td>
+    <input type="password" name="post_pass">
+		</td>
+	</tr>
+	<tr>
+		<td>
+    <h3>Post:</h3>
+	</td>
+		<td>
+    <input type="submit"></input>
+		</td>
+	</tr>
+	</table>
+</form>';
+	}
+?>
+
+
